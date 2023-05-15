c2dc1c632608ed9efb46163927be97b58b7da752
RG214221 <116347578+RG214221@users.noreply.github.com>
2023-05-15 23:32:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
-
-sdk.auth('rnd_YVIFfpurrLrpKlXxYyO3CjvBvkIl');
-sdk.getServices({limit: '20'})
-  .then(({ data }) => console.log(data))
-  .catch(err => console.error(err));
+  const express = require("express");
+  const sdk = require("api")("@render-api/v1.0#dnrc1ulf088q9j");
+  
+  const app = express();
+  const port = 3000;
+  
+  var repos;
+  
+  sdk.auth("rnd_CDTy8UVwLqhLsVLfsMjnkTt939kD");
+  sdk
+    .getServices({ limit: "20" })
+    .then(({ data }) => {
+      repos = data;
+      console.log(data);
+    })
+    .catch((err) => console.error(err));
+  
+  app.get("/", (req, res) => {
+    res.send(repos);
+  });
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on http://localhost:${port}`);
+  });
+  
